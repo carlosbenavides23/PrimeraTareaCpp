75c7f99e6921e078858ad592f057f9e56eb17cb0
carlosbenavides23 <carlosbenavidessandino@gmail.com>
2026-04-08 16:21:58 -0600

    Mostrar lista al usuario

--- ArreglosCpp/ContarElementosMayoresQueUnNumero.cpp
+++ ArreglosCpp/ContarElementosMayoresQueUnNumero.cpp
@@ -7,12 +7,18 @@ int main() {
   int numeros[10] = {45, 60, 30, 75, 67, 80, 10, 90, 50, 65};
   int contador = 0;
 
+  std::cout << "Lista de numeros:" << std::endl;
   for (int i = 0; i < 10; i++) {
-    if (numeros[i] > 50) { // Verificar si el número es mayor que 50
-      contador++; // Incrementar el contador si el número es mayor que 50
+    std::cout << numeros[i] << " ";
+  }
+  std::cout << std::endl << std::endl;
+
+  for (int i = 0; i < 10; i++) {
+    if (numeros[i] > 50) {
+      contador++;
     }
   }
-  // Mostrar el resultado
+
   std::cout << "Cantidad de elementos mayores que 50: " << contador
             << std::endl;
 
